86e272c0ebe12ca82a3baf3a84925a1105382bac
tris <nexus4fortune@gmail.com>
2020-08-10 10:17:48 +0000

    スペース入りと、色名に対応

--- main.go
+++ main.go
@@ -10,7 +10,159 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"testing"
 )
+
+var cssColors = map[string]string{
+	"black":                "#000000",
+	"silver":               "#c0c0c0",
+	"gray":                 "#808080",
+	"white":                "#ffffff",
+	"maroon":               "#800000",
+	"red":                  "#ff0000",
+	"purple":               "#800080",
+	"fuchsia":              "#ff00ff",
+	"green":                "#008000",
+	"lime":                 "#00ff00",
+	"olive":                "#808000",
+	"yellow":               "#ffff00",
+	"navy":                 "#000080",
+	"blue":                 "#0000ff",
+	"teal":                 "#008080",
+	"aqua":                 "#00ffff",
+	"orange":               "#ffa500",
+	"aliceblue":            "#f0f8ff",
+	"antiquewhite":         "#faebd7",
+	"aquamarine":           "#7fffd4",
+	"azure":                "#f0ffff",
+	"beige":                "#f5f5dc",
+	"bisque":               "#ffe4c4",
+	"blanchedalmond":       "#ffebcd",
+	"blueviolet":           "#8a2be2",
+	"brown":                "#a52a2a",
+	"burlywood":            "#deb887",
+	"cadetblue":            "#5f9ea0",
+	"chartreuse":           "#7fff00",
+	"chocolate":            "#d2691e",
+	"coral":                "#ff7f50",
+	"cornflowerblue":       "#6495ed",
+	"cornsilk":             "#fff8dc",
+	"crimson":              "#dc143c",
+	"cyan":                 "#00ffff",
+	"darkblue":             "#00008b",
+	"darkcyan":             "#008b8b",
+	"darkgoldenrod":        "#b8860b",
+	"darkgray":             "#a9a9a9",
+	"darkgreen":            "#006400",
+	"darkgrey":             "#a9a9a9",
+	"darkkhaki":            "#bdb76b",
+	"darkmagenta":          "#8b008b",
+	"darkolivegreen":       "#556b2f",
+	"darkorange":           "#ff8c00",
+	"darkorchid":           "#9932cc",
+	"darkred":              "#8b0000",
+	"darksalmon":           "#e9967a",
+	"darkseagreen":         "#8fbc8f",
+	"darkslateblue":        "#483d8b",
+	"darkslategray":        "#2f4f4f",
+	"darkslategrey":        "#2f4f4f",
+	"darkturquoise":        "#00ced1",
+	"darkviolet":           "#9400d3",
+	"deeppink":             "#ff1493",
+	"deepskyblue":          "#00bfff",
+	"dimgray":              "#696969",
+	"dimgrey":              "#696969",
+	"dodgerblue":           "#1e90ff",
+	"firebrick":            "#b22222",
+	"floralwhite":          "#fffaf0",
+	"forestgreen":          "#228b22",
+	"gainsboro":            "#dcdcdc",
+	"ghostwhite":           "#f8f8ff",
+	"gold":                 "#ffd700",
+	"goldenrod":            "#daa520",
+	"greenyellow":          "#adff2f",
+	"grey":                 "#808080",
+	"honeydew":             "#f0fff0",
+	"hotpink":              "#ff69b4",
+	"indianred":            "#cd5c5c",
+	"indigo":               "#4b0082",
+	"ivory":                "#fffff0",
+	"khaki":                "#f0e68c",
+	"lavender":             "#e6e6fa",
+	"lavenderblush":        "#fff0f5",
+	"lawngreen":            "#7cfc00",
+	"lemonchiffon":         "#fffacd",
+	"lightblue":            "#add8e6",
+	"lightcoral":           "#f08080",
+	"lightcyan":            "#e0ffff",
+	"lightgoldenrodyellow": "#fafad2",
+	"lightgray":            "#d3d3d3",
+	"lightgreen":           "#90ee90",
+	"lightgrey":            "#d3d3d3",
+	"lightpink":            "#ffb6c1",
+	"lightsalmon":          "#ffa07a",
+	"lightseagreen":        "#20b2aa",
+	"lightskyblue":         "#87cefa",
+	"lightslategray":       "#778899",
+	"lightslategrey":       "#778899",
+	"lightsteelblue":       "#b0c4de",
+	"lightyellow":          "#ffffe0",
+	"limegreen":            "#32cd32",
+	"linen":                "#faf0e6",
+	"magenta":              "#ff00ff",
+	"mediumaquamarine":     "#66cdaa",
+	"mediumblue":           "#0000cd",
+	"mediumorchid":         "#ba55d3",
+	"mediumpurple":         "#9370db",
+	"mediumseagreen":       "#3cb371",
+	"mediumslateblue":      "#7b68ee",
+	"mediumspringgreen":    "#00fa9a",
+	"mediumturquoise":      "#48d1cc",
+	"mediumvioletred":      "#c71585",
+	"midnightblue":         "#191970",
+	"mintcream":            "#f5fffa",
+	"mistyrose":            "#ffe4e1",
+	"moccasin":             "#ffe4b5",
+	"navajowhite":          "#ffdead",
+	"oldlace":              "#fdf5e6",
+	"olivedrab":            "#6b8e23",
+	"orangered":            "#ff4500",
+	"orchid":               "#da70d6",
+	"palegoldenrod":        "#eee8aa",
+	"palegreen":            "#98fb98",
+	"paleturquoise":        "#afeeee",
+	"palevioletred":        "#db7093",
+	"papayawhip":           "#ffefd5",
+	"peachpuff":            "#ffdab9",
+	"peru":                 "#cd853f",
+	"pink":                 "#ffc0cb",
+	"plum":                 "#dda0dd",
+	"powderblue":           "#b0e0e6",
+	"rosybrown":            "#bc8f8f",
+	"royalblue":            "#4169e1",
+	"saddlebrown":          "#8b4513",
+	"salmon":               "#fa8072",
+	"sandybrown":           "#f4a460",
+	"seagreen":             "#2e8b57",
+	"seashell":             "#fff5ee",
+	"sienna":               "#a0522d",
+	"skyblue":              "#87ceeb",
+	"slateblue":            "#6a5acd",
+	"slategray":            "#708090",
+	"slategrey":            "#708090",
+	"snow":                 "#fffafa",
+	"springgreen":          "#00ff7f",
+	"steelblue":            "#4682b4",
+	"tan":                  "#d2b48c",
+	"thistle":              "#d8bfd8",
+	"tomato":               "#ff6347",
+	"turquoise":            "#40e0d0",
+	"violet":               "#ee82ee",
+	"wheat":                "#f5deb3",
+	"whitesmoke":           "#f5f5f5",
+	"yellowgreen":          "#9acd32",
+	"rebeccapurple":        "#663399",
+}
 
 // -----------------------------------------------------------------
 
@@ -44,7 +196,7 @@ func NewColor(str string) (Color, error) {
 	}
 
 	// rgb(255,255,255), rgba(255,255,255,100%)
-	re = regexp.MustCompile(`(?i)^rgba?\((\d+),(\d+),(\d+)`)
+	re = regexp.MustCompile(`(?i)^rgba?\(\s*(\d+)[,\s]\s*(\d+)[,\s]\s*(\d+)`)
 	result := re.FindStringSubmatch(str)
 	if len(result) > 0 {
 		r, _ := strconv.Atoi(result[1])
@@ -54,7 +206,7 @@ func NewColor(str string) (Color, error) {
 	}
 
 	// rgb(100%,100%,100%), rgba(100%,100%,100%,100%)
-	re = regexp.MustCompile(`(?i)^rgba?\((\d+)%,(\d+)%,(\d+)%`)
+	re = regexp.MustCompile(`(?i)^rgba?\(\s*(\d+)%[,\s]\s*(\d+)%[,\s]\s*(\d+)%`)
 	result = re.FindStringSubmatch(str)
 	if len(result) > 0 {
 		rp, _ := strconv.Atoi(result[1])
@@ -67,7 +219,7 @@ func NewColor(str string) (Color, error) {
 	}
 
 	// hsl(360,100%,100%), hsla(360,100%,100%,100%), hsla(360,100%,100%/100%)
-	re = regexp.MustCompile(`(?i)^hsla?\((\d+),(\d+)%,(\d+)%`)
+	re = regexp.MustCompile(`(?i)^hsla?\(\s*(\d+)[,\s]\s*(\d+)%[,\s]\s*(\d+)%`)
 	result = re.FindStringSubmatch(str)
 	if len(result) > 0 {
 		h, _ := strconv.Atoi(result[1])
@@ -81,7 +233,7 @@ func NewColor(str string) (Color, error) {
 	}
 
 	// hsl(100%,100%,100%), hsla(100%,100%,100%,100%), hsla(100%,100%,100%/100%)
-	re = regexp.MustCompile(`(?i)^hsla?\((\d+)%,(\d+)%,(\d+)%`)
+	re = regexp.MustCompile(`(?i)^hsla?\(\s*(\d+)%[,\s]\s*(\d+)%[,\s]\s*(\d+)%`)
 	result = re.FindStringSubmatch(str)
 	if len(result) > 0 {
 		h, _ := strconv.Atoi(result[1])
@@ -92,6 +244,16 @@ func NewColor(str string) (Color, error) {
 		if e != nil {
 			return Color{0, 0, 0}, errors.New("invalid argument for convert from hsl")
 		}
+		return Color{r, g, b}, nil
+	}
+
+	// CSS色キーワードが渡されたかも。
+	key := strings.ToLower(str)
+	val, ok := cssColors[key]
+	if ok {
+		r := hex2Decimal(val[1:3])
+		g := hex2Decimal(val[3:5])
+		b := hex2Decimal(val[5:7])
 		return Color{r, g, b}, nil
 	}
 
@@ -216,6 +378,7 @@ func outputColor(str string) {
 // -----------------------------------------------------------------
 
 func init() {
+	testing.Init()
 	flag.Parse()
 }
 
@@ -227,7 +390,7 @@ func run() error {
 
 	// パイプでの入力と、引数による処理を分ける。
 	switch filename {
-	case "", "-":
+	case "", "-": // パイプでの実行時
 		r := os.Stdin
 		s, err := ioutil.ReadAll(r)
 		if err != nil {
@@ -245,17 +408,7 @@ func run() error {
 			outputColor(color)
 		}
 
-		// fmt.Println(str)
 	default:
-		oneColorFlagPtr := flag.Bool("one", false, "output only one color")
-
-		// 1色表示フラグが立っているときは、空白を消して、1色のみ出力する。
-		if *oneColorFlagPtr {
-			str := strings.Join(flag.Args(), "")
-			outputColor(str)
-			return nil
-		}
-
 		// 通常時は渡された色でループする。
 		for _, arg := range flag.Args() {
 			// fmt.Println(arg)
--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// 総合的なテスト。
+// 色々なフォーマットで色を生成するテスト。
 func TestNewColor(t *testing.T) {
 	tests := []struct {
 		str  string
@@ -14,29 +14,46 @@ func TestNewColor(t *testing.T) {
 		{"#F00", Color{255, 0, 0}},
 		{"#03c", Color{0, 51, 204}},
 		{"#AAB0", Color{170, 170, 187}},
-		{"#5G5", Color{0, 0, 0}},
+		{"#5G5", Color{0, 0, 0}}, // ERROR
 
 		{"#AF0532", Color{175, 5, 50}},
 		{"#01975233", Color{1, 151, 82}},
 
 		{"RGB(20,30,40)", Color{20, 30, 40}},
 		{"rgba(160,0,0,50%)", Color{160, 0, 0}},
+		{"rgb(40, 130, 20)", Color{40, 130, 20}},
+		{"RGBA(0, 160, 0, 100%)", Color{0, 160, 0}},
+		{"rgb(240 30 60)", Color{240, 30, 60}},
+		{"rgb(65 180 90 / 1)", Color{65, 180, 90}},
 
 		{"RGB(20%,30%,40%)", Color{51, 76, 102}},
 		{"rgba(90%,100%,0%,50%)", Color{229, 255, 0}},
+		{"rgb(30%, 40%, 20%)", Color{76, 102, 51}},
+		{"RGBA(0%, 90%, 100%, 50%)", Color{0, 229, 255}},
+		{"rgb(100%, 60%, 0)", Color{0, 0, 0}}, // ERROR
+		{"rgb(40% 20% 90%)", Color{102, 51, 229}},
 
 		{"hsl(169,57%,75%)", Color{154, 227, 214}},
 		{"HSLA(345,23%,52%,67%)", Color{160, 104, 118}},
-		{"hsla(7,34%,40%)", Color{136, 75, 67}},
+		{"HSL(169, 57%, 75%)", Color{154, 227, 214}},
+		{"hsla(7, 34%, 40%, 20%)", Color{136, 75, 67}},
+		{"hsl(169 57% 75%)", Color{154, 227, 214}},
 
 		{"hsl(10%,54%,32%)", Color{125, 90, 37}},
 		{"HSLA(57%,43%,56%,10%)", Color{94, 150, 191}},
 		{"hsla(80%,78%,67%/97%)", Color{210, 105, 236}},
+		{"hsl(10%, 54%, 32%)", Color{125, 90, 37}},
+		{"hsl(10%, 54%, 32%, 60%)", Color{125, 90, 37}},
+		{"hsl(10% 54% 32%)", Color{125, 90, 37}},
+		{"hsl(10% 54% 32% / 60%)", Color{125, 90, 37}},
+
+		{"Chocolate", Color{210, 105, 30}},
+		{"TOMATO", Color{255, 99, 71}},
 	}
 
 	for _, tt := range tests {
 		if got, _ := NewColor(tt.str); got != tt.want {
-			t.Errorf("want = %s, got = %s", tt.want, got)
+			t.Errorf("in=%s want=%s, got=%s", tt.str, tt.want, got)
 		}
 	}
 }
